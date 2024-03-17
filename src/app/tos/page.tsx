7ed0d3f3f0a8ad730e4eabb43d8f1ce707f36485
iamdipankarj <iamdipankarj@gmail.com>
2024-03-17 22:59:46 +0530

    Add sample tos

---
+++ src/app/tos/page.tsx
@@ -0,0 +1,18 @@
+export default function TOS() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="container mx-auto px-4 py-6 prose">
+        <h1>Terms of Service</h1>
+
+        <h2>1. Acceptance of Terms</h2>
+        <p>By accessing or using the Platform, you acknowledge that you have read, understood, and agree to be bound by these Terms, as well as any additional terms and policies referenced herein or made available through the Platform.</p>
+
+        <h2>2. Services</h2>
+        <p>The Company provides a platform for users to [describe the services offered]. The Company reserves the right to modify or discontinue any aspect of the Platform at any time without prior notice.</p>
+
+        <h2>3. User Accounts</h2>
+        <p>You may be required to create an account to access certain features of the Platform. You agree to provide accurate and complete information when creating your account and to update such information as necessary. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.</p>
+      </div>
+    </main>
+  )
+}
