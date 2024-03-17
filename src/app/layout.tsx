2a3d328036d876ee5c4561a7a1f02ab5b94e09fb
iamdipankarj <iamdipankarj@gmail.com>
2024-03-17 19:44:53 +0530

    chore: setup a custom theme named signal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning data-theme="cupcake">
+    <html lang="en" suppressHydrationWarning data-theme="signal">
       <body className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>
         {children}
       </body>
