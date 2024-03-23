abaae08b847994186b3fb88225487c70a76f67a7
iamdipankarj <iamdipankarj@gmail.com>
2024-03-24 03:30:04 +0530

    feat: Add markdown based blog.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
-import dayjs from 'dayjs'
 import { Category } from "@/components/category";
+import { getPostBySlug } from "@/lib/mdx";
 
-export default function BlogPost({ params }: { params: { slug: string } }) {
+const getPageContent = async (slug: string) => {
+  const { meta, content } = await getPostBySlug(slug);
+  return {
+    meta,
+    content
+  }
+}
+
+export default async function BlogPost({ params }: { params: { slug: string } }) {
   const categories = ["Technology", "UX Design"];
   const publishedAt = "2022-10-18T14:49:00.000Z";
+  const { content, meta } = await getPageContent(params.slug);
+  const metaData = meta as any;
 
   return (
     <main className="min-h-screen">
-      <div className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8 !pt-0">
+      <div className="container px-8 mx-auto xl:px-5 max-w-screen-lg">
         <div className="mx-auto max-w-screen-md ">
           <div className="flex justify-center">
             <div className="flex gap-3 mt-5">
@@ -21,7 +31,7 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
             </div>
           </div>
           <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight lg:text-4xl lg:leading-snug">
-            A Guide to NextJS and the state of Web Development
+            {metaData.title}
           </h1>
           <div className="mt-3 flex justify-center space-x-3 text-gray-500 ">
             <div className="flex items-center gap-3">
@@ -44,7 +54,7 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
                 </p>
                 <div className="flex items-center space-x-2 text-sm">
                   <time className="text-gray-500 dark:text-gray-400" dateTime={publishedAt}>
-                    {dayjs(publishedAt).format('MMMM D, YYYY')}
+                    {metaData.date}
                   </time>
                   <span>· 2 min read</span>
                 </div>
@@ -52,10 +62,18 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
             </div>
           </div>
         </div>
+        <Image
+          src="/blog/1.jpeg"
+          alt={metaData.title}
+          className="mx-auto mt-6"
+          width={1200}
+          height={708}
+          loading="lazy"
+        />
       </div>
-      <section className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8">
-        <div className="grid gap-10 md:grid-cols-2 lg:gap-10 ">
-          <div>My Post: {params.slug}</div>
+      <section className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-2 lg:py-8">
+        <div className="prose mx-auto lg:prose-xl">
+          {content}
         </div>
       </section>
     </main>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,28 @@
 import { Article } from "@/components/article";
+import { getAllPosts } from "@/lib/mdx";
 
-export default function Blog() {
+export default async function Blog() {
+  const posts = await getAllPosts();
+
   return (
     <main className="min-h-screen">
       <section className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8">
-        <div className="grid gap-10 md:grid-cols-2 lg:gap-10 ">
-          <Article
+        <div className="grid gap-10 md:grid-cols-2 lg:gap-10">
+          {posts.map((post) => (
+            <Article
+              key={post.slug}
+              heading={post.title}
+              thumbnail="/blog/1.jpeg"
+              href={`/blog/${post.slug}`}
+              categories={["Technology", "UX Design"]}
+              authorImage="/blog/avatar1.jpeg"
+              authorName="John Doe"
+              publishedAt="2022-10-21T15:48:00.000Z"
+            >
+              {post.excerpt}
+            </Article>
+          ))}
+          {/* <Article
             heading="A Guide to NextJS and the state of Web Development"
             thumbnail="/blog/1.jpeg"
             href="/blog/a-guide-to-nextjs-and-the-state-of-web-development"
@@ -15,7 +32,7 @@ export default function Blog() {
             publishedAt="2022-10-21T15:48:00.000Z"
           >
             Next.js: React framework for SSR, SSG, and CSR. Features include automatic code splitting, API routes, file-based routing, CSS, and TypeScript support.
-          </Article>
+          </Article> */}
         </div>
       </section>
     </main>
